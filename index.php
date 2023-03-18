efa3c1bd76d83fc8ca85055654e45a0170bb96a2
KaioHenrique900 <kaioneziohenrique@gmail.com>
2023-03-18 10:27:53 -0300

    Updating

--- index.php
+++ index.php
@@ -3,9 +3,7 @@
     <title>Filmes</title>
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <link rel="icon" href="https://s.yimg.com/iu/api/res/1.2/qxRKZiwu1P2P31Uk8vSsXg--~C/YXBwaWQ9eXNwb3J0cztmaT1maWxsO2g9NTAwO3E9MTAwO3c9NTAw/https://s.yimg.com/xe/i/us/sp/v/soccer/teams/20140625/84/500x500/972.png" type="image/jpg">
-
+    
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     
